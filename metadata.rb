f5b48acd87b1b9806824666ad30dd67c83942975
footprns <imankurniawan@gmail.com>
2016-11-26 14:37:07 +0400

    success

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'nagioslogserver'
 maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer_email 'imankurniawan@gmail.com'
 license 'all_rights'
 description 'Installs/Configures nagioslogserver'
 long_description 'Installs/Configures nagioslogserver'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,4 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-remote_file
+remote_file 'nagioslogserver-latest.tar.gz' do
+  source 'http://assets.nagios.com/downloads/nagios-log-server/nagioslogserver-latest.tar.gz'
+end
+
+execute 'extraction_of_installer' do
+  command 'tar xzf nagioslogserver-latest.tar.gz'
+end
+
+execute 'install_nagioslogserver' do
+  command './nagioslogserver/fullinstall'
+end
